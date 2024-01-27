a6c94c38c3a72cd99c0e1fb82aa616c4bf2b3686
Mark Karamyar <5760341+devmarkpro@users.noreply.github.com>
2024-01-27 22:01:13 +0100

    add s3 for obsidian vault

--- main.tf
+++ main.tf
@@ -5,27 +5,11 @@ provider "aws" {
   region = var.region
 }
 
-data "aws_ami" "ubuntu" {
-  most_recent = true
-
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = ["099720109477"] # Canonical
-}
-
-resource "aws_instance" "ubuntu" {
-  ami           = data.aws_ami.ubuntu.id
-  instance_type = var.instance_type
+resource "aws_s3_bucket" "obsidian_bucket" {
+  bucket = "devmarkpro-obsidian-vault"
 
   tags = {
-    Name = var.instance_name
+    Name = "Obsidian Vault"
+    App  = "Obsidian"
   }
 }
