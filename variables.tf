6a97c0a8cbe88ab7f736839489a7407eb93e263b
Mark Karamyar <5760341+devmarkpro@users.noreply.github.com>
2024-01-27 22:02:22 +0100

    remove extra vars

--- variables.tf
+++ variables.tf
@@ -3,3 +3,14 @@ variable "region" {
   description = "AWS region"
   default     = "us-central-1"
 }
+
+variable "instance_type" {
+  description = "Type of EC2 instance to provision"
+  default     = "t2.micro"
+}
+
+variable "instance_name" {
+  description = "EC2 instance name"
+  default     = "Provisioned by Terraform"
+}
+
