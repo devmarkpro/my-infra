feb59e9a35af2a879af8a2331aade6ee777b53f6
Mark Karamyar <5760341+devmarkpro@users.noreply.github.com>
2024-01-27 22:44:18 +0100

    add access policy and role for obsidian

--- main.tf
+++ main.tf
@@ -5,6 +5,8 @@ provider "aws" {
   region = var.region
 }
 
+################ Obsidian ##############
+
 resource "aws_s3_bucket" "obsidian_bucket" {
   bucket = "devmarkpro-obsidian-vault"
 
@@ -12,4 +14,41 @@ resource "aws_s3_bucket" "obsidian_bucket" {
     Name = "Obsidian Vault"
     App  = "Obsidian"
   }
+}
+
+# Define an IAM policy that grants full access to the S3 bucket
+resource "aws_iam_policy" "obsidian_full_access" {
+  name        = "ObsidianFullAccessPolicy"
+  description = "Policy that grants full access to S3 bucket"
+  
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Action = "s3:*",
+      Effect = "Allow",
+      Resource = [aws_s3_bucket.obsidian_bucket.arn, "${aws_s3_bucket.obsidian_bucket.arn}/*"]
+    }]
+  })
+}
+
+# Create an IAM role and attach the policy
+resource "aws_iam_role" "obsidian_admin_role" {
+  name = "ObsidianAdminRole"
+  
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Action = "sts:AssumeRole",
+      Effect = "Allow",
+      Principal = {
+        Service = "ec2.amazonaws.com"
+      }
+    }]
+  })
+}
+
+# Attach the IAM policy to the IAM role
+resource "aws_iam_role_policy_attachment" "s3_admin_attachment" {
+  policy_arn = aws_iam_policy.obsidian_full_access.arn
+  role       = aws_iam_role.obsidian_admin_role.name
 }
--- variables.tf
+++ variables.tf
@@ -4,4 +4,7 @@ variable "region" {
   default     = "eu-central-1"
 }
 
-
+variable "iam_account" {
+  description = "AWS IAM Account"
+  default = "637423330852/devmarkpro"
+}
