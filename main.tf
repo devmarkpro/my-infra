7fd83e9be8024999f4f53958388eeed754c74ec0
Mark Karamyar <5760341+devmarkpro@users.noreply.github.com>
2024-01-28 20:53:06 +0100

    add podcast bucket

--- main.tf
+++ main.tf
@@ -11,7 +11,7 @@ resource "aws_s3_bucket" "obsidian_bucket" {
   bucket = "devmarkpro-obsidian-vault"
 
   tags = {
-    Name = "Obsidian Vault"
+    Name = "App"
     App  = "Obsidian"
   }
 }
@@ -19,7 +19,7 @@ resource "aws_s3_bucket" "obsidian_bucket" {
 # Define an IAM policy that grants full access to the S3 bucket
 resource "aws_iam_policy" "obsidian_full_access" {
   name        = "ObsidianFullAccessPolicy"
-  description = "Policy that grants full access to S3 bucket"
+  description = "Policy that grants full access to obsidian"
   
   policy = jsonencode({
     Version = "2012-10-17",
@@ -48,7 +48,56 @@ resource "aws_iam_role" "obsidian_admin_role" {
 }
 
 # Attach the IAM policy to the IAM role
-resource "aws_iam_role_policy_attachment" "s3_admin_attachment" {
+resource "aws_iam_role_policy_attachment" "obsidian_s3_admin_attachment" {
   policy_arn = aws_iam_policy.obsidian_full_access.arn
   role       = aws_iam_role.obsidian_admin_role.name
+}
+
+
+################ Podcasts ##############
+
+resource "aws_s3_bucket" "podcast_bucket" {
+  bucket = "devmarkpro-podcasts"
+
+  tags = {
+    Name = "App"
+    App  = "Podcast"
+  }
+}
+
+# Define an IAM policy that grants full access to podcast
+resource "aws_iam_policy" "podcast_full_access" {
+  name        = "PodcastFullAccessPolicy"
+  description = "Policy that grants full access to podcast"
+  
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Action = "s3:*",
+      Effect = "Allow",
+      Resource = [aws_s3_bucket.podcast_bucket.arn, "${aws_s3_bucket.podcast_bucket.arn}/*"]
+    }]
+  })
+}
+
+# Create an IAM role and attach the policy
+resource "aws_iam_role" "podcast_admin_role" {
+  name = "PodcastAdminRole"
+  
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Action = "sts:AssumeRole",
+      Effect = "Allow",
+      Principal = {
+        Service = "ec2.amazonaws.com"
+      }
+    }]
+  })
+}
+
+# Attach the IAM policy to the IAM role
+resource "aws_iam_role_policy_attachment" "podcast_s3_admin_attachment" {
+  policy_arn = aws_iam_policy.podcast_full_access.arn
+  role       = aws_iam_role.podcast_admin_role.name
 }
--- variables.tf
+++ variables.tf
@@ -8,3 +8,14 @@ variable "iam_account" {
   description = "AWS IAM Account"
   default = "637423330852/devmarkpro"
 }
+variable "AWS_ACCESS_KEY_ID" {
+  sensitive = true
+  description = "AWS_ACCESS_KEY_ID"
+  nullable = false
+}
+
+variable "AWS_SECRET_ACCESS_KEY" {
+  sensitive = true
+  description = "AWS_SECRET_ACCESS_KEY"
+  nullable = false
+}
